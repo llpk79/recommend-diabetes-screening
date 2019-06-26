01c3ff37e1ab760f94a4c82e1e0c9fc6af9dcfa5
Ryan Herr <ryanherr@Ryans-MBP.zyxel.com>
2019-06-26 14:27:43 -0500

    Add pipenv pipfiles

--- index.py
+++ index.py
@@ -8,14 +8,14 @@ from tabs import intro, predict, explain, evaluate
 style = {'maxWidth': '960px', 'margin': 'auto'}
 
 app.layout = html.Div([
-    dcc.Markdown('# Lending Club interest rates'),
+    dcc.Markdown('# # Lending Club interest rates'),
     dcc.Tabs(id='tabs', value='tab-intro', children=[
         dcc.Tab(label='Intro', value='tab-intro'), 
         dcc.Tab(label='Predict', value='tab-predict'), 
         dcc.Tab(label='Explain', value='tab-explain'), 
-        dcc.Tab(label='Evaluate', value='tab-evaluate')
+        dcc.Tab(label='Evaluate', value='tab-evaluate'),  
     ]),
-    html.Div(id='tabs-content')
+    html.Div(id='tabs-content'), 
 ], style=style)
 
 @app.callback(Output('tabs-content', 'children'),
@@ -25,6 +25,7 @@ def render_content(tab):
     elif tab == 'tab-predict': return predict.layout
     elif tab == 'tab-explain': return explain.layout
     elif tab == 'tab-evaluate': return evaluate.layout
+    elif tab == 'tab-explore': return explore.layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
