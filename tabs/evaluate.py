f9a73338855c07ac6b452c2422d8a65b7c084f97
Paul <pkutrich@gmail.com>
2019-06-28 01:06:59 -0700

    Fix name error

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -14,7 +14,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit ornare
 Phasellus sodales vehicula nisi sit amet tincidunt. Integer mattis, lacus vitae tempor congue, tellus nulla congue lectus, id facilisis enim nunc eu arcu. Suspendisse vehicula metus non urna congue, ac fringilla dolor venenatis. Nulla facilisi. Etiam ornare ipsum id massa dictum, sit amet porttitor neque vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a purus vulputate leo egestas consectetur in nec augue.
 """)
 
-pickled_threshold = load(open('threshold.p', 'rb'))
-threshold = loads(pickled_threshold)
+pickled_threshold = pickle.load(open('threshold.p', 'rb'))
+threshold = pickle.loads(pickled_threshold)
 
 threshold()
