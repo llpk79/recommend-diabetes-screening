ea751be14f3e230b81bba514b323ecff571fa761
Paul <pkutrich@gmail.com>
2019-06-28 10:42:35 -0700

    Add text and images to explain tab. remove explore tab.

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 from app import app, server
-from tabs import intro, predict, explain, evaluate
+from tabs import intro, predict, explain
 
 style = {'maxWidth': '960px', 'margin': 'auto'}
 
@@ -13,7 +13,7 @@ app.layout = html.Div([
         dcc.Tab(label='Intro', value='tab-intro'), 
         dcc.Tab(label='Predict', value='tab-predict'), 
         dcc.Tab(label='Explain', value='tab-explain'), 
-        dcc.Tab(label='Evaluate', value='tab-evaluate'),  
+#         dcc.Tab(label='Evaluate', value='tab-evaluate'),  
     ]),
     html.Div(id='tabs-content'), 
 ], style=style)
@@ -24,7 +24,7 @@ def render_content(tab):
     if tab == 'tab-intro': return intro.layout
     elif tab == 'tab-predict': return predict.layout
     elif tab == 'tab-explain': return explain.layout
-    elif tab == 'tab-evaluate': return evaluate.layout
+#     elif tab == 'tab-evaluate': return evaluate.layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
