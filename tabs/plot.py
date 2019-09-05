1bb63d541b6a5e327da315bfde967162b6de642b
Paulus Kutrich <pkutrich@gmail.com>
2019-09-04 20:13:47 -0700

    use url for csv

--- tabs/plot.py
+++ tabs/plot.py
@@ -12,8 +12,8 @@ class PlotFig(object):
 
     def create_fig(self):
         print("creating figure...")
-        val = pd.read_csv('assets/val_data.csv', 'rb')
-        roc = pd.read_csv('assets/roc_data.csv', 'rb')
+        val = pd.read_csv("https://raw.githubusercontent.com/llpk79/recommend-diabetes-screening/master/assets/val_data.csv")
+        roc = pd.read_csv("https://raw.githubusercontent.com/llpk79/recommend-diabetes-screening/master/assets/roc_data.csv")
         hist_data = val['y_pred_proba1']
         kde = gaussian_kde(hist_data)
         y = kde.pdf(np.linspace(min(hist_data), max(hist_data)))
