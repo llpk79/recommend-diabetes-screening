b1c97af94be46d4c764f244de36cda8543d50ff9
Paulus Kutrich <pkutrich@gmail.com>
2019-09-04 17:07:43 -0700

    try another prefix

--- tabs/plot.py
+++ tabs/plot.py
@@ -12,8 +12,8 @@ class PlotFig(object):
 
     def create_fig(self):
         print("We've gotten this far.")
-        val = pd.read_csv('./val_data.csv')
-        roc = pd.read_csv('roc_data.csv')
+        val = pd.read_csv('/val_data.csv')
+        roc = pd.read_csv('/roc_data.csv')
         hist_data = val['y_pred_proba1']
         kde = gaussian_kde(hist_data)
         y = kde.pdf(np.linspace(min(hist_data), max(hist_data)))
@@ -248,5 +248,6 @@ class PlotFig(object):
 
         # Return chart.
         return fig
+
 
 fig = PlotFig().fig
